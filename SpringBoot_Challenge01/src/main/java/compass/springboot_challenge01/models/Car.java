5a5b46be7ad197058227a3d595c45e1551fa104f
Igor Amaral <igoralamaral@hotmail.com>
2023-09-13 09:39:34 -0300

    feat: starting project, config database and Car model

---
+++ SpringBoot_Challenge01/src/main/java/compass/springboot_challenge01/models/Car.java
@@ -0,0 +1,70 @@
+package compass.springboot_challenge01.models;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chassi_id")
+    private Long chassiId;
+
+    @NotBlank(message = "Insert a valid model")
+    private String model;
+    @NotBlank(message = "Chose one of this brands: Ford, Chevrolet, BMW or Volvo")
+    private String brand;
+    @NotBlank(message = "Insert a valid color")
+    private String color;
+    @NotBlank(message = "Insert a valid fabrication year in format: year/year")
+    @Column(name = "fabrication_year")
+    private String fabricationYear;
+
+    public Car(String model, String brand, String color, String fabricationYear) {
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+        this.fabricationYear = fabricationYear;
+    }
+
+    public Car() {
+
+    }
+
+    public Long getChassiId() {
+        return chassiId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public void setFabricationYear(String fabricationYear) {
+        this.fabricationYear = fabricationYear;
+    }
+}
